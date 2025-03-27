b87ddbeefc081987257aea4a123dcf6e0101a70f
rohit-jangra-dx <rjangradev@gmail.com>
2025-03-27 09:25:36 +0530

    fix: typo in contact route

---
+++ frontend/src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+"use client "
+import React from 'react'
+import ProtectedRoute from '../../../components/ProtectedRoute'
+
+function Contect() {
+  return (
+    <div>
+      <ProtectedRoute>
+        <h1>Contact</h1>
+    </ProtectedRoute>
+    </div>
+  )
+}
+
+export default Contect
